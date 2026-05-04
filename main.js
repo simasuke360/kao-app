cf422457ec151f6fae14840a671170a73139ac99
simasuke360 <simasuke118@gmail.com>
2026-05-04 14:42:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-
-// 2.5秒後に表示
 setTimeout(() => {
   if (Notification.permission === "default") {
-    document.getElementById("notifyUI").style.display = "block";
+    if (confirm("アップデート通知を受け取りますか？")) {
+      Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+          alert("通知ONになりました 👍");
+        }
+      });
+    }
   }
 }, 2500);
-
-// ボタン押したとき
-function enableNotify() {
-  Notification.requestPermission().then(permission => {
-    if (permission === "granted") {
-      alert("通知ON 👍");
-    }
-  });
-
-  document.getElementById("notifyUI").style.display = "none";
-}
-
 
 
 
